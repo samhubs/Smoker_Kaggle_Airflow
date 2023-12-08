44db5da6e807bac3b43abb33ad3651eb34480f18
sameer <Sameer>
2023-12-08 16:30:11 -0700

    fix bug in inspect.py

--- dags/data_processing_cleaning.py
+++ dags/data_processing_cleaning.py
@@ -45,7 +45,7 @@ def data_processing_and_cleaning():
         train = pd.read_sql('SELECT * FROM train_table', engine)
         test = pd.read_sql('SELECT * FROM test_table', engine)
 
-        # Drop the 'id' column and duplicate values in the training set 
+        # Drop the 'id' column 
         train = train.drop(['id'], axis=1)
         test = test.drop(['id'], axis=1)
         train = train.drop_duplicates()
@@ -56,24 +56,22 @@ def data_processing_and_cleaning():
 
     @task(task_id='preprocess_data_task')
     def preprocess_data():
-        # Read data from the database
         train = pd.read_sql('SELECT * FROM train_clean_table', engine)
-
-        # Let's split features from target 
-        X = train.drop(['smoking'], axis=1)
+        
         Y = train['smoking']
+        X = train.drop(['smoking'], axis=1)
 
         X.to_csv('/opt/airflow/data/X.csv', sep=',', index=False)
         Y.to_csv('/opt/airflow/data/Y.csv', sep=',', index=False)
 
     @task(task_id='save_data_task')
-    def save_data(preprocess_data):
+    def save_data():
         pass
 
     loaded_data = load_data()
     cleaned_data = clean_data()
-    preprocessed_data = preprocess_data(cleaned_data)
-    save_data(preprocessed_data) 
+    preprocessed_data = preprocess_data()
+    save_data() 
 
 
 dag = data_processing_and_cleaning()
